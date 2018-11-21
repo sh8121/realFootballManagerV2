228c41aad35557d6cf7deafc7cfc29318bc87431
부상훈 <sboo@ebaykorea.com>
2018-11-21 15:57:09 +0900

    add team related biz

---
+++ src/actions/team.actions.js
@@ -0,0 +1,15 @@
+import { teamConstants } from "../constants/team.constants";
+
+export const teamActions = {
+
+}
+
+function register(){
+    return (dispatch) => {
+
+    };
+
+    function request(){ return {type: teamConstants.REGISTER.REQUEST}}
+    function success(){ return {type: teamConstants.REGISTER.SUCCESS}}
+    function failure(){ return {type: teamConstants.REGISTER.FAILURE}}
+}
---
+++ src/constants/team.constants.js
@@ -0,0 +1,7 @@
+export const teamConstants = {
+    REGISTER: {
+        REQUEST: "TEAM_REGISTER_REQUEST",
+        SUCCESS: "TEAM_REGISTER_SUCCESS",
+        FAILURE: "TEAM_REGISTER_FAILURE"
+    }
+}
